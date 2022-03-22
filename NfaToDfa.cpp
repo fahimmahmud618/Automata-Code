b624267533053f2e11520e06f640a0c2b00a99fd
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2022-03-22 17:17:32 +0600

    Update NfaToDfa.cpp

--- NfaToDfa.cpp
+++ NfaToDfa.cpp
@@ -2,6 +2,9 @@
 #define MAX 100
 using namespace std;
 string nfa_transition_0[MAX], nfa_transition_1[MAX];
+string dfa_new_or_done_state[MAX],dfa_state[MAx],dfa_transition_0[MAX], dfa_transition_1[MAX];
+int num_dfa_new_or_done_state=0, num_of_DFA_state=0;
+int uni_array[MAX];
 
 void print_nfa(int state_num)
 {
@@ -10,6 +13,43 @@ void print_nfa(int state_num)
     {
         cout<<(char)(i+65)<<"\t"<<nfa_transition_0[i]<<"\t"<<nfa_transition_1[i]<<endl;
     }
+}
+int calc_string_len(string input)
+{
+    int i;
+    for(i=0;input[i]!='\0';i++);
+    return i;
+}
+
+int pick_state(string taken_str)
+{
+    int len = calc_string_len(taken_str),t=0;
+    cout<<"len is: "<<len<<endl;
+    for(int i=0; i<len;i++)
+    {
+        if((taken_str[i]==',')||(taken_str[i]=='{')||(taken_str[i]=='}'))
+        {
+
+        }
+        else
+        {
+            uni_array[t]=((int)(taken_str[i]))-65;
+            cout<<"==>"<<uni_array[t];
+            t++;
+        }
+
+    }
+    return t;
+}
+void convert_nfa_dfa()
+{
+    dfa_state[0]=(char)(65);
+    dfa_transition_0[0]= nfa_transition_0[0];
+    dfa_transition_1[0]= nfa_transition_1[0];
+    num_of_DFA_state++;
+
+
+
 }
 int main()
 {
@@ -33,6 +73,7 @@ int main()
         //cout<<"value of i"<<i;
     }
     print_nfa(num_of_NFA_state);
+
 
 
 }
